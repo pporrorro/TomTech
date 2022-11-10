9f2e6cc85cb4c64d1b0a87c00e53139e8cf11ddc
lim du hyoung <limduh2@naver.com>
2022-11-10 14:24:42 +0900

    구매자화면 cs파일들 생성
    오류2개 왜날까요?

---
+++ FinalTombuyScreen/SecondTombuyScreen/OrderList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTombuyScreen
+{
+    internal class OrderList
+    {
+        public string Order_Number = "";
+        public string Product_Code = "";
+        public int Order_Qty = 0;
+        public string User_Number = "";
+        public string User_Name = "";
+        public string Region_Name = "";
+        public string Address = "";
+        public string Order_Time = "";
+    }
+}
---
+++ FinalTombuyScreen/SecondTombuyScreen/ProductManage.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace SecondTombuyScreen
+{
+    partial class ProductManage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnOrderbasketCancel = new System.Windows.Forms.Button();
+            this.btnShowOrderbasket = new System.Windows.Forms.Button();
+            this.tableLayoutPanel_pList = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.btnPutInformation = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtOrderList = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.36601F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.63399F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.66771F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1337, 657);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel_pList, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.55118F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.448819F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(640, 651);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnOrderbasketCancel);
+            this.panel2.Controls.Add(this.btnShowOrderbasket);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 592);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(634, 56);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnOrderbasketCancel
+            // 
+            this.btnOrderbasketCancel.Location = new System.Drawing.Point(312, 5);
+            this.btnOrderbasketCancel.Name = "btnOrderbasketCancel";
+            this.btnOrderbasketCancel.Size = new System.Drawing.Size(194, 43);
+            this.btnOrderbasketCancel.TabIndex = 83;
+            this.btnOrderbasketCancel.Text = "장바구니 취소";
+            this.btnOrderbasketCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnShowOrderbasket
+            // 
+            this.btnShowOrderbasket.Location = new System.Drawing.Point(74, 7);
+            this.btnShowOrderbasket.Name = "btnShowOrderbasket";
+            this.btnShowOrderbasket.Size = new System.Drawing.Size(221, 42);
+            this.btnShowOrderbasket.TabIndex = 82;
+            this.btnShowOrderbasket.Text = "장바구니 담기";
+            this.btnShowOrderbasket.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel_pList
+            // 
+            this.tableLayoutPanel_pList.ColumnCount = 1;
+            this.tableLayoutPanel_pList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_pList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_pList.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel_pList.Name = "tableLayoutPanel_pList";
+            this.tableLayoutPanel_pList.RowCount = 1;
+            this.tableLayoutPanel_pList.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_pList.Size = new System.Drawing.Size(634, 583);
+            this.tableLayoutPanel_pList.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.groupBox7, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(649, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.12598F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 67.87402F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(685, 651);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.btnPutInformation);
+            this.groupBox7.Controls.Add(this.textBox2);
+            this.groupBox7.Controls.Add(this.textBox1);
+            this.groupBox7.Controls.Add(this.label4);
+            this.groupBox7.Controls.Add(this.label7);
+            this.groupBox7.Location = new System.Drawing.Point(3, 3);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(597, 184);
+            this.groupBox7.TabIndex = 84;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "구매자 정보";
+            // 
+            // btnPutInformation
+            // 
+            this.btnPutInformation.Location = new System.Drawing.Point(77, 119);
+            this.btnPutInformation.Name = "btnPutInformation";
+            this.btnPutInformation.Size = new System.Drawing.Size(152, 48);
+            this.btnPutInformation.TabIndex = 4;
+            this.btnPutInformation.Text = "입력";
+            this.btnPutInformation.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(396, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "주소";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "구매자";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtOrderList);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 212);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(679, 436);
+            this.panel1.TabIndex = 85;
+            // 
+            // txtOrderList
+            // 
+            this.txtOrderList.Location = new System.Drawing.Point(23, 27);
+            this.txtOrderList.Multiline = true;
+            this.txtOrderList.Name = "txtOrderList";
+            this.txtOrderList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOrderList.Size = new System.Drawing.Size(574, 392);
+            this.txtOrderList.TabIndex = 85;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 12);
+            this.label3.TabIndex = 86;
+            this.label3.Text = "장바구니 정보";
+            // 
+            // ProductManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 657);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ProductManage";
+            this.Text = "ProductManage";
+            this.Load += new System.EventHandler(this.ProductManage_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnOrderbasketCancel;
+        private System.Windows.Forms.Button btnShowOrderbasket;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_pList;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Button btnPutInformation;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtOrderList;
+        private System.Windows.Forms.Label label3;
+    }
+}
