6741c7a66df170a5416c6fd1bb3e73e8fb7e6b37
PARK YONG HEE <109491199+PARKYNH@users.noreply.github.com>
2022-11-26 11:36:27 +0900

    .

--- Jerry/ModernUI-SnapWindow-v0.2/DashBoard.cs
+++ Jerry/ModernUI-SnapWindow-v0.2/DashBoard.cs
@@ -120,12 +120,61 @@ namespace ModernUI_SnapWindow
 
         private void LiveChartSource() // 지도 함수 데이터 기능
         {
+            string strConn = "Server=222.98.255.30;Database=black_sheep;Uid=root;Pwd=qmffortlq;";
+
+            Dictionary<string, double> value = new Dictionary<string, double>();
+
+
+            using (MySqlConnection conn = new MySqlConnection(strConn))
+            {
+                conn.Open();
+                //MySqlCommand cmd = new MySqlCommand("select * from black_sheep.OrderList", conn);
+                string query = "SELECT B.idRegion_Number as Region, count(A.Order_number) as count " +
+                    "FROM black_sheep.OrderList AS A " +
+                    "INNER JOIN black_sheep.Region_Number AS B " +
+                    "ON A.Region_Name = B.Region_Name " +
+                    "GROUP BY Region ";
+
+
+
+//                SELECT B.idRegion_Number as Region, count(A.Order_number) as count
+//FROM black_sheep.OrderList AS A
+//INNER JOIN black_sheep.Region_Number AS B
+//ON A.Region_Name = B.Region_Name
+//GROUP BY Region
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                
+
+
+                MySqlDataReader reader;
+                try
+                {
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string count = reader.GetString("count");
+                        string Region = reader.GetString("Region");
+                        value[Region] = Convert.ToDouble(count);
+                        //this.chart1.Series["Product_Price"].Points.AddXY(reader.GetString("Product_Name"), reader.GetInt32("Product_Price"));
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+
+
             LiveCharts.WinForms.GeoMap geoMap = new LiveCharts.WinForms.GeoMap();
             Random random = new Random();
-            Dictionary<string, double> value = new Dictionary<string, double>();
-            value["2003"] = 0;
-            value["2037"] = 37;
-            value["1999"] = 99;
+           // Dictionary<string, double> value = new Dictionary<string, double>();
+           // value["2003"] = 8;
+           // value["2037"] = 37;
+           // value["1999"] = 99;
             geoMap.HeatMap = value;
             geoMap.Source = $"{Application.StartupPath}\\South Korea.xml";
             this.panel13.Controls.Add(geoMap);
